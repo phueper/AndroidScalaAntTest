d5fd9cc9fd1f4f9f9c22795fd61de11a4edd3a15
Pattrick Hueper <phueper@hueper.net>
2009-08-09 21:36:27 +0200

    more fun with scala... implicit conversion, anonymous function (closure?)...
    Signed-off-by: Pattrick Hueper <phueper@hueper.net>

--- src/net/hueper/android/test/AndroidAntScalaTest.scala
+++ src/net/hueper/android/test/AndroidAntScalaTest.scala
@@ -2,21 +2,38 @@ package net.hueper.android.test
 
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
-import _root_.android.widget.TextView
+import _root_.android.widget.{TextView, ScrollView}
 import _root_.android.view.View
+import _root_.android.hardware.{Sensor, SensorManager}
 
 class AndroidAntScalaTest extends Activity {
-	implicit def textViewConverter(v:View):TextView = {v.asInstanceOf[TextView] }
-  
+    // converter from View to any view Type (e.g. for findViewById)
+	implicit def view2TextView(v:View):TextView = {v.asInstanceOf[TextView] }
+    implicit def view2ScrollView(v:View):ScrollView = {v.asInstanceOf[ScrollView] }
+    // converter for anonymous func to Runnable (e.g. for post())
+    implicit def func2Runnable(func: ()=>Unit):Runnable = {
+      new Runnable() {
+    	  def run() {
+    	    func()
+    	  }
+      }
+       
+    }
+	
 	override def onCreate(savedInstanceState: Bundle) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.main)
-		var mTextView:TextView = findViewById(R.id.TextView01)
-		mTextView.setText("blablub\nblubdiblub")
+		var textView:TextView = findViewById(R.id.TextView01)
+		var scrollView:ScrollView = findViewById(R.id.ScrollView01)
+		textView.setText("blablub\nblubdiblub")
 		for (i <- 1 to 100) {
-			mTextView.append(i + "\n")
-			println("log test" + i)
+			textView.append(i + "\n")
+			println("log test " + i)
 		}
+		scrollView.post(()=>{
+		  scrollView.fullScroll(View.FOCUS_DOWN)
+		  ()
+		})
 		true
 	}
 
