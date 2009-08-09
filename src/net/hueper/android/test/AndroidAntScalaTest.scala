3040791cc87cd91d9a9e71baa3cbe5d11bc9cb4e
Pattrick Hueper <phueper@hueper.net>
2009-08-09 17:48:59 +0200

    some ui tests...
    Signed-off-by: Pattrick Hueper <phueper@hueper.net>

--- src/net/hueper/android/test/AndroidAntScalaTest.scala
+++ src/net/hueper/android/test/AndroidAntScalaTest.scala
@@ -2,12 +2,22 @@ package net.hueper.android.test
 
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
+import _root_.android.widget.TextView
+import _root_.android.view.View
 
 class AndroidAntScalaTest extends Activity {
+	implicit def textViewConverter(v:View):TextView = {v.asInstanceOf[TextView] }
+  
 	override def onCreate(savedInstanceState: Bundle) {
 		super.onCreate(savedInstanceState)
-		// use a bit of Scala's stdlib, just to show off
-		setContentView(List(R.layout.main) head)
+		setContentView(R.layout.main)
+		var mTextView:TextView = findViewById(R.id.TextView01)
+		mTextView.setText("blablub\nblubdiblub")
+		for (i <- 1 to 100) {
+			mTextView.append(i + "\n")
+			println("log test" + i)
+		}
+		true
 	}
 
 }
