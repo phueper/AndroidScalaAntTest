5974215249db0ffff56e239cf52455810ce79d38
Pattrick Hueper <phueper@hueper.net>
2009-08-21 15:55:54 +0200

    add an actor
    Signed-off-by: Pattrick Hueper <phueper@hueper.net>

--- src/net/hueper/android/test/AndroidAntScalaTest.scala
+++ src/net/hueper/android/test/AndroidAntScalaTest.scala
@@ -8,6 +8,8 @@ import _root_.android.view.View.OnClickListener
 import _root_.android.widget.AdapterView
 import _root_.android.widget.AdapterView.OnItemSelectedListener
 import _root_.android.hardware.{Sensor, SensorManager}
+import _root_.scala.actors.Actor
+import _root_.scala.actors.Actor._
 
 class AndroidAntScalaTest extends Activity {
 	// converter from View to any view Type (e.g. for findViewById)
@@ -36,13 +38,6 @@ class AndroidAntScalaTest extends Activity {
 			}
 	}
 
-/*	implicit def func2OnItemSelectedListener0(funcs:(()=>Unit, ()=>Unit)) = {
-		new OnItemSelectedListener() {
-			def onItemSelected(p:AdapterView[_], v:View, pos:int, id:long) = funcs._1.apply
-			def onNothingSelected(p:AdapterView[_]) = funcs._2.apply
-		}
-	}
-*/
 	override def onCreate(savedInstanceState: Bundle) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.main)
@@ -50,6 +45,8 @@ class AndroidAntScalaTest extends Activity {
 		val scrollView:ScrollView = findViewById(R.id.ScrollView01)
 		val spinner:Spinner = findViewById(R.id.Spinner01)
 		val button:Button = findViewById(R.id.Button01)
+		val buttonActor:ButtonActor = new ButtonActor
+		buttonActor.start
 
 		textView.setText("")
 /*
@@ -63,7 +60,6 @@ class AndroidAntScalaTest extends Activity {
 		val sensors = sm.getSensorList(Sensor.TYPE_ALL)
 		val it = sensors.iterator()
 		val sensorNames:java.util.List[String] = new java.util.ArrayList()
-		sensorNames.add("bla")
 		while (it.hasNext) {
 			val name = it.next().getName()
 			textView.append(name + "\n")
@@ -73,15 +69,12 @@ class AndroidAntScalaTest extends Activity {
 		spinner.setAdapter(sensorAdapter)
 
 		spinner.setOnItemSelectedListener( (p:AdapterView[_], v:View, pos:int, id:long)=>{
-			textView.append(spinner.getSelectedItem().toString())
+			textView.append(spinner.getSelectedItem().toString() + "\n")
 			scrollViewUpdate(scrollView)
 			},
 			(p:AdapterView[_])=>{textView.append("nothing selected\n");scrollViewUpdate(scrollView)})
 
-/*		spinner.setOnItemSelectedListener( ()=>{textView.append("item selected\n");scrollViewUpdate(scrollView)}, 
-				()=>{textView.append("nothing selected\n");scrollViewUpdate(scrollView)})
-*/
-  		button.setOnClickListener((v:View)=>{textView.append("click\n");scrollViewUpdate(scrollView)})
+		button.setOnClickListener((v:View)=>{textView.append("click\n");buttonActor!"msg";scrollViewUpdate(scrollView)})
 		scrollViewUpdate(scrollView)
 		true
 	}
@@ -93,5 +86,21 @@ class AndroidAntScalaTest extends Activity {
 			() // must return Unit
 		})
 	}
+ 
+	class ButtonActor extends Actor {
+		private val textView:TextView = findViewById(R.id.TextView01)
+		private val scrollView:ScrollView = findViewById(R.id.ScrollView01)
+		private val spinner:Spinner = findViewById(R.id.Spinner01)
+		def act() {
+			loop {
+				receive {
+					case msg => textView.append("msg received: " + msg.toString() + "\n");scrollViewUpdate(scrollView); 
+				}
+			}
+		}
+	}
 
 }
+
+
+
